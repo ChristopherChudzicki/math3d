a55725daa2a7dc82a8aeac0990d3da09ecb8af7d
Chris Chudzicki <christopher.chudzicki@gmail.com>
2016-12-25 15:59:06 -0500

    add onLongPress directive

--- docs/math3d_app.js
+++ docs/math3d_app.js
@@ -21,6 +21,42 @@ app.directive('compileTemplate', ["$compile", "$parse", function($compile, $pars
     }
 }]);
 
+// https://gist.github.com/BobNisco/9885852
+// Add this directive where you keep your directives
+app.directive('onLongPress', function($timeout) {
+	return {
+		restrict: 'A',
+		link: function($scope, $elm, $attrs) {
+			$elm.bind('mousedown', function(evt) {
+				// Locally scoped variable that will keep track of the long press
+				$scope.longPress = true;
+
+				// We'll set a timeout for 600 ms for a long press
+				$timeout(function() {
+					if ($scope.longPress) {
+						// If the touchend event hasn't fired,
+						// apply the function given in on the element's on-long-press attribute
+						$scope.$apply(function() {
+							$scope.$eval($attrs.onLongPress)
+						});
+					}
+				}, 600);
+			});
+
+			$elm.bind('mouseup', function(evt) {
+				// Prevent the onLongPress event from firing
+				$scope.longPress = false;
+				// If there is an on-touch-end function attached to this element, apply it
+				if ($attrs.onTouchEnd) {
+					$scope.$apply(function() {
+						$scope.$eval($attrs.onTouchEnd)
+					});
+				}
+			});
+		}
+	};
+})
+
 app.controller('saveCtrl', ['$scope', function($scope){
         $scope.saveURL = function(){
         var url = math3d.saveSettingsAsURL();
