fee375925080e23801bfec13def87b79937c0bbe
Jason Chen <jasonchen6699@gmail.com>
2017-04-26 14:38:48 -0400

    Add app configuration for secret_key and database

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 import logging
 from whitenoise import WhiteNoise
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
+
+# Configurations
+app.config.update(
+    SECRET_KEY=os.environ["SECRET_KEY"],
+    SQLALCHEMY_DATABASE_URL=os.environ["DATABASE_URL"],
+)
 
 @app.route('/')
 def index():
