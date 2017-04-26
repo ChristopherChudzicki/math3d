9887238bb7abdfcc3977bafb0c8a1cfda8a61a36
Jason Chen <jasonchen6699@gmail.com>
2017-04-26 15:31:44 -0400

    Configure bcrypt and sqlalchemy
    Fix typo DATABASE_URL --> DATABASE_URI.
    Don't know what URI is but it's the correct setting

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import logging
 from whitenoise import WhiteNoise
 from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
@@ -9,8 +11,11 @@ app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 # Configurations
 app.config.update(
     SECRET_KEY=os.environ["SECRET_KEY"],
-    SQLALCHEMY_DATABASE_URL=os.environ["DATABASE_URL"],
+    SQLALCHEMY_DATABASE_URI=os.environ["DATABASE_URL"],
 )
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
