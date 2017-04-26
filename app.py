b29bdc6603bd2630593f7bf11987ebe8fad37002
Jason Chen <jasonchen6699@gmail.com>
2017-04-26 10:43:14 -0400

    Using WhiteNoise for static files serving

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import logging
-
+from whitenoise import WhiteNoise
 from flask import Flask, render_template, redirect
+
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 
 @app.route('/')
 def index():
