ef88b6c9b960dcf35bf89dd2c4abd924477bab42
Jason Chen <jasonchen6699@gmail.com>
2017-04-26 16:41:53 -0400

    Correct typo for modification tracking settings
    Forgot to add the 's' in MODIFICATIONS

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 app.config.update(
     SECRET_KEY=os.environ["SECRET_KEY"],
     SQLALCHEMY_DATABASE_URI=os.environ["DATABASE_URL"],
-    SQLALCHEMY_TRACK_MODIFICATION=False,
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
 
 db = SQLAlchemy(app)
