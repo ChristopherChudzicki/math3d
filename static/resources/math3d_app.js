3e657fba6f76881f9e5b3b302ca5d1532527cd2c
Jason Chen <kbtpodifo@gmail.com>
2017-05-13 16:15:46 -0400

    Add MIT license to files
    Also add my name to the files I contribute to.

--- static/resources/math3d_app.js
+++ static/resources/math3d_app.js
@@ -1,6 +1,7 @@
 // MIT License
 //
 // Copyright (c) 2017 Christopher Chudzicki
+// Copyright (c) 2017 Luming Chen
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
