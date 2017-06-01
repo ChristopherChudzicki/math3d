2f92da9d1848cdc8609840dd09ea91b45a7a0600
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-05-31 21:28:45 -0400

    fix DATABASE_URL

--- set_local_vars.sh
+++ set_local_vars.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export SECRET_KEY='super_secret'
-export DATABASE_URL='postgresql://localhost/wordcount_dev'
+export DATABASE_URL='postgresql://localhost/math3d'
 export FLASK_APP='app.py'
